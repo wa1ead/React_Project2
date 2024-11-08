8f41ef6bbbaed86f8e87b9e119be2e7197f9b9c2
wa1ead <waliddraa2@gmail.com>
2024-11-08 11:30:42 +0100

    creating players data file

---
+++ src/players.js
@@ -0,0 +1,38 @@
+export const players = [
+  {
+    name: "Cristiano Ronaldo",
+    age: 39,
+    nationality: "🇵🇹",
+    team: "Real Madrid",
+    jerseyNumber: 7,
+    image:
+      "https://i.pinimg.com/736x/96/3d/64/963d643c81ac09fa9beb117346a1b12c.jpg",
+  },
+  {
+    name: "Leo Messi",
+    age: 38,
+    nationality: "🇦🇷",
+    team: "FC Barcelona",
+    jerseyNumber: 10,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQExG7YrZAsqGDR1B7Cd2gH-gnPp18-1U9GgQ&s",
+  },
+  {
+    name: "Mohsine Moutaouali",
+    age: 40,
+    nationality: "🇲🇦",
+    team: "RAJA CA",
+    jerseyNumber: 5,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/2/2d/Mohsine_Moutouali_02-01-2014.jpg",
+  },
+  {
+    name: "Soufiane Rahimi",
+    age: 31,
+    nationality: "🇲🇦",
+    team: "RAJA CA",
+    jerseyNumber: 21,
+    image:
+      "https://sport.le360.ma/resizer/v2/6TNVVJGHZNIYJBIONAQETGOEYA.jpg?auth=d993cbca42f17220799ce260fef12f54f6afcce9713335c9b0131d738f59c75e&smart=true&width=1216&height=684",
+  },
+];
